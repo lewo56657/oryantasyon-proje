108ae878333111b5070d838682d55ba52aad275b
lewo56657 <96915201+lewo56657@users.noreply.github.com>
2021-12-31 22:05:37 +0300

    Add files via upload

---
+++ Defa.java
@@ -0,0 +1,13 @@
+
+package pkg5.defa;
+import java.util.Scanner;
+public class Defa {
+ public static void main(String[] args) {
+   int   i=5;
+      while(i>0)
+      {
+        System.out.println("meloş");
+         i=i-1;   
+      }
+    }   
+}
