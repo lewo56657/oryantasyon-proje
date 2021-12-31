050a64ec33b15af47e7b4cce34308dae55baa91d
lewo56657 <96915201+lewo56657@users.noreply.github.com>
2021-12-31 21:49:52 +0300

    Add files via upload

--- Diziornek4.java
+++ Diziornek4.java
@@ -1,16 +1,13 @@
 
-package diziornek4;
+package diziornek44;
 import java.util.Scanner;
-public class Diziornek4 {
-
+public class Diziornek44;
     public static void main(String[] args) {
-      
-        
-      String s1=new String("LEWO");
-      
-     String  s2= (" ELÇİÇEK");
-      
-      
+     Scanner klavye= new Scanner(System.in); 
+        System.out.println("birinci cümleyi giriniz");
+      String s1=klavye.nextLine ();
+        System.out.println("ikinci cümleyi giriniz");  
+     String  s2=klavye.nextLine();
         System.out.println(s1.concat(s2));
     }
     
