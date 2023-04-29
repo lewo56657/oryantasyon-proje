75460ecb917df89cdaf1ba2099d0343b77b79fb0
lewo56657 <96915201+lewo56657@users.noreply.github.com>
2023-04-29 10:58:03 +0300

    Add files via upload

---
+++ kütüphaneler.py
@@ -0,0 +1,24 @@
+'''
+import string
+import matplotlib #veri analizi
+import Caffe # derin öğrenme ( yüz tanıma,araç takibi)
+import  catboots # sınıflama ve makina öğrenmesi
+import pybrain  # makina öğrenmesi 
+import opencv # görüntü işleme
+import seabor # veri anlazizi ve görselleştirme
+import speechRecognition # ses tanıma
+import spacy #- doğal dil
+import bokeh # görsel tasarım
+import NLTK # doğal dil işleme,konuşmayı word a dökme
+import tensorflow  # yapay sinir ağı ,matemariksel işlemler
+import Theano #ileri düzey matematiksel ifadeleri işleme 
+import beautifulSoup # html vb belgeleri işelme
+import sckit-learn# veri madenciliği
+import statsmodels # istatislik değerli tahmin 
+import Pillow # grafik  ve görsel işleme
+import mpmath # real ve komplex sayılarla işlem
+import gensim # makina öğrenmesi ve modellem
+import reques # web üzerinde veri işleme 
+import pyglet # oyun işleme ve tasarımı
+import anaconda # yeni başlayanlar için alıştırma
+'''
